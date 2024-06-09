01b3b851560d7abd7a1ecbbe772f437ad8dcf1f7
Mohammadshoyebb <141633689+Mohammadshoyebb@users.noreply.github.com>
2024-06-09 15:07:24 +0530

    update

--- SubarraySumsDivisibleByK.java
+++ SubarraySumsDivisibleByK.java
@@ -74,3 +74,29 @@ public class SubarraySumsDivisibleByK {
         System.out.println(solution.subarraysDivByK(nums2, k2)); // Output: 0
     }
 }
+
+
+//=======================================FASTER  METHOD=============================================
+/*
+ * class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int count=0,sum=0,rem;
+        int md[]=new int[k];
+        for(int i=0;i<nums.length;i++)
+        {
+            sum+=nums[i];
+            rem=sum%k;
+            if(rem<0)
+            rem+=k;
+            md[rem]++;
+        }
+        for(int i=0;i<k;i++)
+        {
+            if(md[i]>1)
+            count+=(md[i]*(md[i]-1))/2;
+        }
+        count+=md[0];
+        return count;
+    }
+}
+ */
